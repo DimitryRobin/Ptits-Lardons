944fb352fc6a7c3bfa92d7acbb9a376d3b8392cd
SGShare <jordan.jamain@hotmail.fr>
2015-11-06 17:27:12 +0100

    affiche liste
    erreur dans l'ecriture des affiche_liste dans les deux fichiers

--- modif_enfant.php
+++ modif_enfant.php
@@ -13,7 +13,7 @@ $site->style='perso';
 
 $controleur=new controleur();
 $site-> right_sidebar=$site-> rempli_right_sidebar();
-$site-> left_sidebar=$controleur->affiche_liste_famille('Modif');
+$site-> left_sidebar=$controleur->affiche_liste_enfant('Modif');
 if (isset($_POST["nom_checkbox"])){
 foreach ($_POST["nom_checkbox"] as $index => $value){
 	$site-> left_sidebar=$controleur->retourne_formulaire_enfant('Modif',$value);
